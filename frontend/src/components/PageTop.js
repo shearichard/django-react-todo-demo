9a5dea913f34e6cffe88be28cbcc181dc8e81f2c
Richard Shea <rshea@thecubagroup.com>
2024-08-23 17:01:43 +1200

    Tweak PageTop wrt alert

--- frontend/src/components/PageTop.js
+++ frontend/src/components/PageTop.js
@@ -5,8 +5,18 @@ import DjangoLogo from "./DjangoLogo";
 import ReactLogo from "./ReactLogo";
 import InfoCircle from "./InfoCircle";
 import Github from "./Github";
+import { UncontrolledAlert } from "reactstrap";
 
 export default class PageTop extends Component {
+  constructor(props) {
+	super(props);
+	this.state = {
+	  showInfo: false 
+	};
+  }
+  showInfo(){
+      alert("showing info")
+  }
   render() {
     const tableStyle = {
         align: "center",
@@ -20,30 +30,42 @@ export default class PageTop extends Component {
     const tweakHeader = {
         display:"inherit",
     }
+    const infoRowController = {
+
+    }
 	//const { toggle, onSave } = this.props;
 	return (
-        <table style={tableStyle}>
-            <tbody>
-                <tr>
-                    <td>
-                        <ReactLogo height="5em" width="5em"  />
-                        &nbsp;
-                        <DjangoLogo height="8em" width="5em"  />
-                    </td>
-                    <td>
-                        <span style={floatRight}><InfoCircle height="2em" width="2em" /></span>
-                        &nbsp;
-                        <span style={floatRight}><Github height="2em" width="2em" /></span>
-                    </td>
-                </tr>
-                <tr>
-                    <td colSpan={2}>
-                        <h2 className="text-grey text-uppercase text-center my-4" style={tweakHeader}>React/Django Demonstrator</h2>
-                        <h3 className="text-grey text-uppercase text-left my-4">Todos</h3>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <>
+            <table style={tableStyle}>
+                <tbody>
+                    <tr>
+                        <td>
+                            <ReactLogo height="5em" width="5em"  />
+                            &nbsp;
+                            <DjangoLogo height="8em" width="5em"  />
+                        </td>
+                        <td>
+                            <span onClick={this.showInfo} style={floatRight}><InfoCircle height="2em" width="2em" /></span>
+                            &nbsp;
+                            <a href="https://github.com/shearichard/django-react-todo-demo" target="_blank" rel="noreferrer"><span style={floatRight}><Github height="2em" width="2em" /></span></a>
+                        </td>
+                    </tr>
+                    <tr style={infoRowController}>
+                        <td colSpan={2}>
+                            <UncontrolledAlert color="info" fade={false}>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            </UncontrolledAlert>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colSpan={2}>
+                            <h3 className="text-grey text-uppercase text-center my-4" style={tweakHeader}>React/Django Demonstrator</h3>
+                            <h4 className="text-grey text-uppercase text-left my-4">Todos</h4>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </>
 	);
   }
 }
