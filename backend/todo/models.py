c74521cfe5e1249ebb2b6068d6bf6442d1e754ed
Richard Shea <rshea@thecubagroup.com>
2024-10-23 21:58:44 +1300

    Fix a wrongly named __str__ function in the ToDo model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,9 +2,9 @@
 from django.db import models
 
 class Todo(models.Model):
-  title = models.CharField(max_length=120)
-  description = models.TextField()
-  completed = models.BooleanField(default=False)
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
 
-  def _str_(self):
-    return self.title
+    def __str__(self):
+        return str(self.task)
