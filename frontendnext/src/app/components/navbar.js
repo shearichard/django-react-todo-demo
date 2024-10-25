6481276118c6df81f9d1f1d518c8558a0371cffe
Richard Shea <rshea@thecubagroup.com>
2024-10-25 17:01:49 +1300

    Remove the hamburger menu as the elements were rendering on the wrong side of the screen and it looked weird

--- frontendnext/src/app/components/navbar.js
+++ frontendnext/src/app/components/navbar.js
@@ -1,23 +1,22 @@
 import styles from "../page.module.css";
 export default function Navbar() {
-    return (
-        <nav className="navbar bg-primary" data-bs-theme="dark">
-                <div className="container">
-                    <a className="navbar-brand" href="#">Next/Django Demo</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="/mytodos">My Todos</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-    );
+	    return (
+		            <nav className="navbar bg-primary" data-bs-theme="dark">
+		                <div className="container">
+		                    <a className="navbar-brand" href="#">Next/Django Demo</a>
+		                    <ul className="navbar-nav ms-auto d-flex flex-row">
+		                        <li className="nav-item me-3">
+		                            <a className="nav-link" href="/">Home</a>
+		                        </li>
+		                        <li className="nav-item me-3">
+		                            <a className="nav-link" href="/mytodos">My Todos</a>
+		                        </li>
+		                        <li className="nav-item me-3">
+		                            <a className="nav-link" href="/about">About</a>
+		                        </li>
+		                    </ul>
+		                </div>
+		            </nav>
+		        );
 }
+
