73cc753b6a0675102fae3fa782724ddad7345f70
Richard Shea <rshea@thecubagroup.com>
2024-11-02 22:25:41 +1300

    A change to models.py that will probably need reverting

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -2,10 +2,52 @@
 from django.db import models
 
 
-class Todo(models.Model):
-    title = models.CharField(max_length=120)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
+class Category(models.Model):
+    title = models.CharField(max_length=20, primary_key=True, unique=True, blank=False)
+    deletable = models.BooleanField(default=True)
+
+    '''
+    class Meta:
+        verbose_name = "To Do Category"
+        verbose_name_plural = "To Do Categories"
+    '''
 
     def __str__(self):
         return str(self.title)
+
+
+    def delete(self, *args, **kwargs):
+        # Prevent deletion if the object is marked as non-deletable
+        if not self.deletable:
+            raise ValueError("This object cannot be deleted.")
+        #
+        super().delete(*args, **kwargs)
+
+
+    def save(self, *args, **kwargs):
+        # Ensure the title is stored in uppercase
+        if self.title:
+            self.title = self.title.upper()
+        #
+        super().save(*args, **kwargs)
+
+
+
+
+'''
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    should_be_completed_by_date = models.DateTimeField(null=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    last_updated = models.DateTimeField(auto_now=True, editable=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, default="N/A")
+
+    class Meta:
+        verbose_name = "To Do"
+        verbose_name_plural = "To Do"
+
+    def __str__(self):
+        return str(self.title)
+'''
