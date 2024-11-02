c9915095a7310c11bcd2c8a6c111331c8d8f4ecc
Richard Shea <rshea@thecubagroup.com>
2024-11-02 22:24:52 +1300

    A change to admin.py that will probably need reverting.

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+'''
 from .models import Todo
+'''
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'deletable')
+
+'''
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'completed')
-
+    list_display = ('title', 'description', 'is_completed', 'should_be_completed_by_date', 'created', 'last_updated', 'category')
+'''
 
 # Register your models here.
+admin.site.register(Category, CategoryAdmin)
+'''
 admin.site.register(Todo, TodoAdmin)
+'''
