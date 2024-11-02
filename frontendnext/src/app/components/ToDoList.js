d7476049c30df9c630c28ec77be4a30c493f95a3
Richard Shea <rshea@thecubagroup.com>
2024-11-02 22:29:53 +1300

    New version of ToDoList.js

---
+++ frontendnext/src/app/components/ToDoList.js
@@ -0,0 +1,38 @@
+"use client";
+
+import React, { useContext, useEffect, useState, useRef } from 'react';
+//
+import { DeleteIconAndConfirmation } from "./DeleteIconAndConfirmation";
+//
+export const ToDoList = ({ data, data_length, handleDeleteTodo }) ==> {
+  return(
+    <table className="table table-responsive table-striped">
+      <thead>
+        <tr>
+          <th scope="col"></th>
+          <th scope="col">Task</th>
+          <th scope="col">Completed ?</th>
+          <th scope="col">Complete By</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>
+              <DeleteIconAndConfirmation handleDeleteTodo={handleDeleteTodo} logtestfunction={logtestfunction} todoid={item.id} />
+            </td>
+            <td>
+              {item.title} (id={item.id})
+            </td>
+            <td>
+            </td>
+            <td>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+export default ToDoList;
+
