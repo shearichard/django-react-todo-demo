7dd717a6a859784231b813300ba1fe34f4118f23
Richard Shea <rshea@thecubagroup.com>
2024-11-09 13:39:00 +1300

    Provide rudimentary tests for all the components other than the ToDoList

--- frontendnext/src/App.test.jsx
+++ frontendnext/src/App.test.jsx
@@ -1,4 +1,8 @@
 import Footer from "./app/components/Footer";
+import Navbar from "./app/components/Navbar";
+import DeleteIconAndConfirmation from "./app/components/DeleteIconAndConfirmation";
+import ToDoList from "./app/components/ToDoList";
+import ToggleCompletedIconAndConfirmation from "./app/components/ToggleCompletedIconAndConfirmation";
 
 import { describe, it, expect } from 'vitest';
 
@@ -19,7 +23,7 @@ describe('something truthy and falsy', () => {
     });
 });
 
-//First attempt at component testing
+// Test footer
 describe('Footer', () => {
     it('renders all properties', () => {
         render(<Footer />);
@@ -27,3 +31,37 @@ describe('Footer', () => {
         expect(screen.getByText('© 2024 Richard Shea. All rights reserved.')).toBeInTheDocument();
     });
 });
+// Test Navbar
+describe('Navbar', () => {
+    it('renders all properties', () => {
+        render(<Navbar />);
+        expect(screen.getByText('Next/Django Demo')).toBeInTheDocument();
+        expect(screen.getByText('Home')).toBeInTheDocument();
+        expect(screen.getByText('My Todos')).toBeInTheDocument();
+        expect(screen.getByText('About')).toBeInTheDocument();
+    });
+});
+// Test DeleteIconAndConfirmation
+describe('DeleteIconAndConfirmation', () => {
+    it('renders all properties', () => {
+        render(<DeleteIconAndConfirmation />);
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+})
+// Test ToggleCompletedIconAndConfirmation
+// TBD: This test needs extending to render the component in both states.
+// this may be useful for guidance on doing that 
+// https://stackoverflow.com/questions/53119123/react-testing-library-test-styles-specifically-background-image
+describe('ToggleCompletedIconAndConfirmation', () => {
+    it('renders all properties', () => {
+        render(<ToggleCompletedIconAndConfirmation />);
+        expect(screen.getByRole('button')).toBeInTheDocument();
+    });
+})
+// Test ToDoList
+describe.skip('ToDoList', () => {
+    it('renders all properties', () => {
+        render(<ToDoList />);
+    });
+})
+
