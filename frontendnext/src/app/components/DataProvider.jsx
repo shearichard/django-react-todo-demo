de3fb94f5df101267347dad43f8a8ef608864fed
Richard Shea <rshea@thecubagroup.com>
2024-11-22 10:03:38 +1300

    Centralise specification of scheme/host/port value

--- frontendnext/src/app/components/DataProvider.jsx
+++ frontendnext/src/app/components/DataProvider.jsx
@@ -2,6 +2,8 @@
 import React, { createContext, useState } from 'react';
 import axios from 'axios';
 
+const BACKEND_SCHEME_HOST_PORT = 'http://localhost:8000'
+
 export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
@@ -12,7 +14,7 @@ export const DataProvider = ({ children }) => {
     //debugger
     console.log("fetchTodos A");
     try {
-      const response = await axios.get('http://localhost:8000/api/v1/todos/');
+      const response = await axios.get(`${BACKEND_SCHEME_HOST_PORT}/todo/api/v1/todos/`);
       console.log("fetchTodos B");
       console.log(response.data);
       setData(response.data);
@@ -26,7 +28,7 @@ export const DataProvider = ({ children }) => {
   // Fetch a single to-do item by ID
   const fetchTodo = async (id) => {
     try {
-      const response = await axios.get(`http://localhost:8000/api/v1/todos/${id}/`);
+      const response = await axios.get(`${BACKEND_SCHEME_HOST_PORT}/todo/api/v1/todos/${id}/`);
       return response.data;
     } catch (error) {
       console.error('Error fetching todo:', error);
@@ -37,19 +39,19 @@ export const DataProvider = ({ children }) => {
   // Create a new to-do item
   const addTodo = async (newItem) => {
     try {
-      console.log("addTodo A")	    
-      console.log(newItem)	    
-      console.log("addTodo B")	    
-      const response = await axios.post('http://localhost:8000/api/v1/todos/', newItem, {
+      console.log("addTodo A")
+      console.log(newItem)
+      console.log("addTodo B")
+      const response = await axios.post(`${BACKEND_SCHEME_HOST_PORT}/todo/api/v1/todos/`, newItem, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
-      console.log("addTodo C")	    
+      console.log("addTodo C")
       console.log(response)
-      console.log("addTodo D")	    
+      console.log("addTodo D")
       console.log(response.data)
-      console.log("addTodo E")	    
+      console.log("addTodo E")
       setData((prevData) => [...prevData, response.data]);
     } catch (error) {
       console.error('Error adding todo:', error);
@@ -59,7 +61,7 @@ export const DataProvider = ({ children }) => {
   // Delete a to-do item by ID
   const deleteTodo = async (id) => {
     try {
-      await axios.delete(`http://localhost:8000/api/v1/todos/${id}/`);
+      await axios.delete(`${BACKEND_SCHEME_HOST_PORT}/todo/api/v1/todos/${id}/`);
       setData((prevData) => prevData.filter((item) => item.id !== id));
     } catch (error) {
       console.error('Error deleting todo:', error);
@@ -68,31 +70,31 @@ export const DataProvider = ({ children }) => {
   // Update a to-do item by ID (PATCH)
   const updateTodo = async (id, updatedFields) => {
     try {
-      console.log("updateTodo A")	    
-      console.log(updatedFields)	    
-      console.log("updateTodo B")	    
-      const response = await axios.patch(`http://localhost:8000/api/v1/todos/${id}/`, updatedFields, {
+      console.log("updateTodo A")
+      console.log(updatedFields)
+      console.log("updateTodo B")
+      const response = await axios.patch(`${BACKEND_SCHEME_HOST_PORT}/todo/api/v1/todos/${id}/`, updatedFields, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
-      console.log("updateTodo C")	    
+      console.log("updateTodo C")
       console.log(response.data)
-      console.log("updateTodo D")	    
-			setData(function(prevData) {
-				return prevData.map(function(item) {
-					if (item.id === id) {
-						console.log("updateTodo D1")	    
-						return response.data;
-					} else {
-						console.log("updateTodo D2")	    
-						return item;
-					}
-				});
-			});
-      console.log("updateTodo E")	    
-			console.log(data)
-      console.log("updateTodo F")	    
+      console.log("updateTodo D")
+      setData(function(prevData) {
+        return prevData.map(function(item) {
+          if (item.id === id) {
+            console.log("updateTodo D1")
+            return response.data;
+          } else {
+            console.log("updateTodo D2")
+            return item;
+          }
+        });
+      });
+      console.log("updateTodo E")
+      console.log(data)
+      console.log("updateTodo F")
     } catch (error) {
       console.error('Error updating todo:', error);
     }
