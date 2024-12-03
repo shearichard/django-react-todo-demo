dbd492f1ccfeff529ac5b3fa2f86c2353a5918cf
Richard Shea <rshea@thecubagroup.com>
2024-12-03 16:57:44 +1300

    Add a route so that after a user is created via accounts/signup the redirect does not result in a 404

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,19 @@
 # backend/urls.py
+from django.http import HttpResponse
 from django.contrib import admin
 from django.urls import path, include
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # OpenAPI schema
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
+    path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # Redoc UI
+    path('accounts/profile/', lambda request: HttpResponse("Profile page placeholder - Temporary page until other infrastructure completed")),
+
 ]
 #
