d05dd37c3b406c789a5f28e9325b45221c0113c2
Richard Shea <rshea@thecubagroup.com>
2024-12-04 10:52:31 +1300

    Tweaking the view_django_allauth_openapi_spec.sh shell script

--- backend/doco/django-allauth-openapi-spec/view_django_allauth_openapi_spec.sh
+++ backend/doco/django-allauth-openapi-spec/view_django_allauth_openapi_spec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker run -p 5000:8080 --name swaggerui \
 	-v ./openapi.yaml:/openapi/openapi.yaml \
-	-e SWAGGER_JSON=/openapi/openapi.yaml \
-	swaggerapi/swagger-ui
+        -e SWAGGER_JSON=/openapi/openapi.yaml \
+        swaggerapi/swagger-ui
