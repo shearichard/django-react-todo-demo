db694ac7ac8ae026bbc610a8d2facfd8bc7db187
Richard Shea <rshea@thecubagroup.com>
2024-12-04 16:13:15 +1300

    Tweak backend.urls to make reading it easier

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -10,10 +10,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/', lambda request: HttpResponse("Profile page placeholder - Temporary page until other infrastructure completed")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # OpenAPI schema
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # Redoc UI
-    path('accounts/profile/', lambda request: HttpResponse("Profile page placeholder - Temporary page until other infrastructure completed")),
-
 ]
 #
