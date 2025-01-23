c6b693dde6519ae42e4bc418fad17afb8d63e142
Richard Shea <rshea@thecubagroup.com>
2025-01-24 09:32:53 +1300

    Temporarily remove the drf_spectactular routing definitions

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('todo/', include('todo.urls')),
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', lambda request: HttpResponse("Profile page placeholder - Temporary page until other infrastructure completed")),
+]
+'''
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # OpenAPI schema
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
     path('api/docs/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),  # Redoc UI
-]
-#
+'''
