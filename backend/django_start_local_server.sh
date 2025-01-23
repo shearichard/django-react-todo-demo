b94946e0a4633955809833472bb660d1a5c00e7c
Richard Shea <rshea@thecubagroup.com>
2025-01-24 08:59:13 +1300

    Change address of dev server from localhost to 127.0.0.1 in the startup script used in dev

--- backend/django_start_local_server.sh
+++ backend/django_start_local_server.sh
@@ -3,5 +3,5 @@ if [[ -z "$PIPENV_ACTIVE" && -z "$VIRTUAL_ENV" ]]; then
     echo "Is the virtual enviroment active ? Neither PIPENV_ACTIVE nor VIRTUAL_ENV is set. Stopping script." >&2
     return 1  # Stop script if neither variable exists
 fi
-#python manage.py runserver --settings backend.settings.local  127.0.0.1:8000
-python manage.py runserver --settings backend.settings.local  localhost:8000
+python manage.py runserver --settings backend.settings.local  127.0.0.1:8000
+#python manage.py runserver --settings backend.settings.local  localhost:8000
