227e5c58d365ac735717ac95a9af5649a3f51708
Richard Shea <rshea@thecubagroup.com>
2025-01-24 18:11:45 +1300

    Fix typo in backend.urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('accounts/profile/', lambda request: HttpResponse("Profile page placeholder - Temporary page until other infrastructure completed")),
     path("_allauth/", include("allauth.headless.urls")),
+]
 '''
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),  # OpenAPI schema
     path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),  # Swagger UI
