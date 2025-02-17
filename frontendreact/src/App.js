e591f5f86eff5377497fc5dd4b1740508ea479c4
Richard Shea <rshea@thecubagroup.com>
2025-02-18 08:26:13 +1300

    Tweak home page text

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -10,7 +10,7 @@ function App() {
             {/* Main Content */}
             <div className="container mt-5 flex-grow-1">
                 <div className="jumbotron">
-                    <h1 className="display-4">Welcome to the OAuth for React/Django Demo App</h1>
+                    <h1 className="display-4">OAuth for React/Django</h1>
                     <p className="lead">
                         This is a 'todo' app to demonstrate the use of OAuth in a React/Django app. 
                     </p>
